b4c2254d52dfef687a1148a02d4e9d427bf84fca
Ahmed Jerid <ahmed.jerid@olbati.com>
2017-07-17 17:10:24 +0200

    Complete User Service  test level 2

--- src/test/java/tudu/service/impl/Level2UserServiceImplMockitoTest.java
+++ src/test/java/tudu/service/impl/Level2UserServiceImplMockitoTest.java
@@ -6,22 +6,48 @@ import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.annotation.ExpectedException;
 import tudu.domain.User;
 import tudu.service.UserAlreadyExistsException;
 
 import javax.persistence.EntityManager;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 public class Level2UserServiceImplMockitoTest {
+
+    User user;
+
+    @Mock
+    EntityManager entityManager;
+
+    @InjectMocks
+    UserServiceImpl userService = new UserServiceImpl();
+
+    @Before
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+        user = new User();
+        user.setLogin("test_user");
+        user.setFirstName("First name");
+        user.setLastName("Last name");
+    }
 
     /*
     * Vérifier que l on récupère bien uneRuntimeException en sortie de updateUser si la methode Merge de l entityManager leve une RuntimeException
     * Méthode : updateUser
     */
-    @Test
+    @Test(expected = RuntimeException.class)
     public void when_an_exception_is_thrown_by_entityManager_then_rethrow() {
-     }
+        //given
+        given(entityManager.merge(user)).willThrow(new RuntimeException());
+        //when
+        userService.updateUser(user);
+        //then
+
+
+    }
 
     /*
     LES METHODES SUIVANTES SONT UNIQUEMENT LA POUR APPRENDRE LA SYNTAXE. CES TESTS SONT EXTREMEMENTS FRAGILES, A MANIPULER AVEC PRECAUTIONS !
@@ -32,8 +58,18 @@ public class Level2UserServiceImplMockitoTest {
     Méthode : createUser
     */
     @Test
-    public void when_creation_of_a_new_user_then_4_calls_to_entityManager_persist()  {
-      }
+    public void when_creation_of_a_new_user_then_4_calls_to_entityManager_persist() throws UserAlreadyExistsException {
+        //given
+        //when
+        userService.createUser(user);
+        userService.createUser(user);
+        userService.createUser(user);
+        userService.createUser(user);
+        //then
+        verify(entityManager, times(4)).persist(user);
+
+
+    }
 
     @Test
     /*
@@ -42,16 +78,30 @@ public class Level2UserServiceImplMockitoTest {
     Méthode : createUser
     */
 
-    public void when_creation_of_a_new_user_then_between_2_and_5_calls_to_entityManager_persist() {
-        }
+    public void when_creation_of_a_new_user_then_between_2_and_5_calls_to_entityManager_persist() throws UserAlreadyExistsException {
+
+        userService.createUser(user);
+        userService.createUser(user);
+        //then
+        verify(entityManager, atLeast(2)).persist(user);
+        verify(entityManager, atMost(5)).persist(user);
+
+    }
 
     /*
     Type : Test Comportement
     Vérifier que si l'utilisateur existe, la methode persiste n'a jamais été appelée
     Méthode : createUser
     */
-    @Test
-    public void when_the_login_already_exist_then_persist_never_called_1()  {
+    // a verifier
+    @Test(expected = UserAlreadyExistsException.class)
+    public void when_the_login_already_exist_then_persist_never_called_1() throws UserAlreadyExistsException {
+        //given
+        given(entityManager.find(User.class, user.getLogin())).willReturn(user);
+        //when
+        userService.createUser(user);
+        verify(entityManager, never()).persist(user);
+
 
     }
 
@@ -60,9 +110,16 @@ public class Level2UserServiceImplMockitoTest {
     Vérifier que si l'utilisateur existe, la methode persiste n'a jamais été appelée - 2eme alternative avec verifyNoMoreInteractions
     Méthode : createUser
     */
+//a verfier
+    @Test(expected = UserAlreadyExistsException.class)
+    public void when_the_login_already_exist_then_persist_never_called_2() throws UserAlreadyExistsException {
+        //given
+        given(entityManager.find(User.class, user.getLogin())).willReturn(user);
+        //when
+        userService.createUser(user);
+        //then
+        verifyNoMoreInteractions(entityManager);
 
-    @Test
-    public void when_the_login_already_exist_then_persist_never_called_2() {
 
     }
 
