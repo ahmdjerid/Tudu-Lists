9d95219d92c9a4cc7f4248bba609d3c01f83abb5
Ahmed Jerid <ahmed.jerid@olbati.com>
2017-07-17 17:10:11 +0200

    Complete User Service  test level 1

--- src/test/java/tudu/service/impl/Level1UserServiceImplMockitoTest.java
+++ src/test/java/tudu/service/impl/Level1UserServiceImplMockitoTest.java
@@ -11,18 +11,24 @@ import tudu.service.UserAlreadyExistsException;
 
 import javax.persistence.EntityManager;
 
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 public class Level1UserServiceImplMockitoTest {
 
+    User user;
 
-    User user = new User();
     @Mock
     EntityManager entityManager;
+
     @InjectMocks
     UserServiceImpl userService = new UserServiceImpl();
 
     @Before
     public void before() {
         MockitoAnnotations.initMocks(this);
+        user =new User();
         user.setLogin("test_user");
         user.setFirstName("First name");
         user.setLastName("Last name");
@@ -36,11 +42,14 @@ public class Level1UserServiceImplMockitoTest {
     */
     @Test
     public void find_user_should_return_the_user() {
+
         //given
-
+        given(entityManager.find(User.class, "test_user")).willReturn(user);
         //when
-
+        User foundUser = userService.findUser("test_user");
         //then
+        assertThat(foundUser).isEqualTo(user);
+
     }
 
     @Test
@@ -52,10 +61,11 @@ public class Level1UserServiceImplMockitoTest {
     */
     public void update_user_should_call_entityManager_merge() {
         //given
-
+        given(entityManager.merge(user)).willReturn(user);
         //when
-
+        userService.updateUser(user);
         //then
+        verify(entityManager).merge(user);
     }
 
     @Test
@@ -66,10 +76,12 @@ public class Level1UserServiceImplMockitoTest {
     */
     public void user_should_be_retrieved() {
         //given
-
+        given(entityManager.find(User.class, "toto")).willReturn(user);
         //when
-
+        User foundUser = userService.findUser("toto");
         //then
+        verify(entityManager).find(User.class, "toto");
+
     }
 
 
@@ -77,14 +89,12 @@ public class Level1UserServiceImplMockitoTest {
     Vérifier qu'une exception de type ObjectRetrievalFailureException est bien levée si l entityManager find renvoie null
     Méthode : findUser
     */
-    @Test
+    @Test(expected = ObjectRetrievalFailureException.class)
     public void error_should_be_thrown_when_a_user_is_not_found() {
         //given
-
+        given(entityManager.find(User.class, "toto")).willReturn(null);
         //when
-
-        //then
-
+        userService.findUser("toto");
     }
 
     /*
@@ -92,13 +102,12 @@ public class Level1UserServiceImplMockitoTest {
     Méthode : createUser
     */
 
-    @Test
+    @Test(expected = UserAlreadyExistsException.class)
     public void exception_should_be_thrown_when_creating_an_already_existed_user() throws UserAlreadyExistsException {
         //given
-
+        given(entityManager.find(User.class, user.getLogin())).willReturn(user);
         //when
-
-        //then
+        userService.createUser(user);
     }
 
     /*
@@ -108,11 +117,13 @@ public class Level1UserServiceImplMockitoTest {
     */
     @Test
     public void new_user_should_be_saved() throws UserAlreadyExistsException {
+
         //given
-
         //when
-
+        userService.createUser(user);
         //then
+        verify(entityManager).persist(user);
+
     }
 
 
